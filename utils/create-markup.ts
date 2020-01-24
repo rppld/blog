4636cf399e9dccf0d5b24fc527bffb74e5bdf5f5
Philipp Rappold <philipprappold@me.com>
2020-01-24 22:33:41 +0100

    Replace showdown with marked

--- utils/create-markup.ts
+++ utils/create-markup.ts
@@ -1,17 +1,29 @@
-import showdown from 'showdown'
+import marked from 'marked'
 
 interface Options {
   renderInline?: boolean
 }
 
 export default function createMarkup(markdown: string, options: Options = {}) {
-  const converter = new showdown.Converter({ noHeaderId: true })
-  let html = converter.makeHtml(markdown)
+  const renderer = new marked.Renderer()
 
   if (options.renderInline) {
-    // Remove <p> tags
-    html = html.replace(/(<p[^>]+?>|<p>|<\/p>)/gim, '')
+    // Render input as is, without wrapping <p> tags
+    renderer.paragraph = text => text
   }
+
+  const config = {
+    renderer,
+    gfm: true,
+    breaks: true,
+    headerIds: true,
+    mangle: true,
+    smartypants: true,
+    xhtml: true,
+  }
+
+  // Parse Markdown
+  const html = marked(markdown, config)
 
   return {
     __html: html,
