791274eb5fecd7047b6561b11c18341d8f50ca2b
Philipp Rappold <philipprappold@me.com>
2020-01-27 18:33:09 +0100

    Add lang attribute on html tag

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
