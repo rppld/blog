d421f00f87d32fd7d9f5e6e0cd709c9f3efa335d
Philipp Rappold <philipprappold@me.com>
2023-07-14 20:01:37 +0200

    Revert "update type styles"
    This reverts commit e6e644889c9f47465d658e61a64816ff60e16fb9.

--- data/page.ts
+++ data/page.ts
@@ -4,5 +4,5 @@ export const data: Data["page"] = {
   title: "Philipp Rappold, Software Engineer",
   description:
     "Design-minded software engineer specialized in building large-scale JavaScript applications.",
-  smallprint: `Copyright © ${new Date().getFullYear()} Philipp Rappold. The site is built with Next.js + Tailwind CSS and hosted with Vercel. Uses Vercel Analytics for privacy-friendly insights. The typeface is Mona Sans.`,
+  smallprint: `Copyright © ${new Date().getFullYear()} Philipp Rappold. The site is built with Next.js + Tailwind CSS and hosted with Vercel. Uses Vercel Analytics for privacy-friendly insights.`,
 };
