4d17e8dcc56c3b63c7fcdf42e410d4c156a4d095
Philipp Rappold <philipprappold@me.com>
2023-07-14 20:01:37 +0200

    Revert "add Mona font"
    This reverts commit 1ab8ae94fd37ad79480e6f5c385753acd45dce08.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
-import localFont from "next/font/local";
 import { data } from "../data";
 import "./globals.css";
-
-const mona = localFont({
-  src: "./Mona-Sans.woff2",
-  display: "swap",
-  variable: "--font-mona",
-});
 
 export const metadata = {
   title: data.page.title,
@@ -20,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={mona.variable}>
+    <html lang="en">
       <body className="bg-[#f8f8f8] dark:bg-[#181818] antialiased">
         {children}
       </body>
