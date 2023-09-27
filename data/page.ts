c653ba220fe092d93a345e9eee96769c8c02c806
Philipp Rappold <philipp@rppld.com>
2023-09-27 22:47:51 +0200

    fix typo

--- data/page.ts
+++ data/page.ts
@@ -1,7 +1,7 @@
 import { Data } from "./index";
 
 export const data: Data["page"] = {
-  title: "Philipp Rappold,UI Engineer",
+  title: "Philipp Rappold, UI Engineer",
   description:
     "UI engineer specialized in building large-scale JavaScript applications.",
   smallprint: `Copyright © ${new Date().getFullYear()} Philipp Rappold. The site is built with Next.js + Tailwind CSS and hosted with Vercel. Uses Vercel Analytics for privacy-friendly insights.`,
